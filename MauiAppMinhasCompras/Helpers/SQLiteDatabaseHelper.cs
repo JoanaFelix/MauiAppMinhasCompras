2cf69f779b6daca5c1247a3644d34dddb1e636e6
Joana <Joana@JOANAFELIX>
2025-03-24 12:14:41 -0300

    Finalização do básico do aplicativo

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -35,7 +35,7 @@ namespace MauiAppMinhasCompras.Helpers
         }
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE Descricao LIKE '%" + "%'";
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%" + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
